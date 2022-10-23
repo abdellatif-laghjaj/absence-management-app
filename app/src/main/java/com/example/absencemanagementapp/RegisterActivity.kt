683d6684d2aba352a1b5c34a6aaf4e3466a85c5b
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-23 18:09:45 +0100

    add IDs to views

--- app/src/main/java/com/example/absencemanagementapp/RegisterActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/RegisterActivity.kt
@@ -1,11 +1,28 @@
 package com.example.absencemanagementapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var login_tv: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        //full screen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        login_tv = findViewById(R.id.login_tv)
+        login_tv.setOnClickListener {
+            Intent(this, LoginActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
     }
 }
