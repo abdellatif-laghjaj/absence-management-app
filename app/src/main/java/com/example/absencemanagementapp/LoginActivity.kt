e80494cb20dbb7088d91d84f8cbe53953c54f39b
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-05 22:55:24 +0100

    login user

--- app/src/main/java/com/example/absencemanagementapp/LoginActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/LoginActivity.kt
@@ -4,18 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.example.absencemanagementapp.models.User
 import com.google.android.material.textfield.TextInputEditText
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.FirebaseDatabase
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var register_tv: TextView
     private lateinit var email_et: TextInputEditText
     private lateinit var password_et: TextInputEditText
+    private lateinit var login_btn: Button
 
-    //Firebase
-    val database = Firebase.database
+    private lateinit var database: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -35,11 +37,57 @@ class LoginActivity : AppCompatActivity() {
                 finish()
             }
         }
+
+        login_btn.setOnClickListener {
+            val email = email_et.text.toString()
+            val password = password_et.text.toString()
+
+            if (validateInputs()) {
+                login(email, password)
+            }
+        }
     }
 
-    fun initViews() {
+    private fun login(email: String, password: String) {
+        database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("students")
+        ref.child(email).get().addOnSuccessListener {
+            if (it.exists()) {
+                val user = it.getValue(User::class.java)
+                if (user?.password == password) {
+                    Intent(this, MainActivity::class.java).also {
+                        startActivity(it)
+                        finish()
+                    }
+                } else {
+                    Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "User doesn't exist", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun initViews() {
         register_tv = findViewById(R.id.register_tv)
         email_et = findViewById(R.id.email_et)
         password_et = findViewById(R.id.password_et)
+        login_btn = findViewById(R.id.login_btn)
+    }
+
+    private fun validateInputs(): Boolean {
+        val email = email_et.text.toString()
+        val password = password_et.text.toString()
+        return when {
+            email.isEmpty() -> {
+                email_et.error = "Email is required"
+                false
+            }
+            password.isEmpty() -> {
+                password_et.error = "Password is required"
+                false
+            }
+            else -> true
+        }
     }
 }
