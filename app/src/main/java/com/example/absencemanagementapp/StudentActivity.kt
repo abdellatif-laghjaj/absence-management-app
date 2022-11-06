85017f584db7755ecde8d7f733c7136087c1d568
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-06 17:14:01 +0100

    welcome user

--- app/src/main/java/com/example/absencemanagementapp/StudentActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/StudentActivity.kt
@@ -2,10 +2,33 @@ package com.example.absencemanagementapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.absencemanagementapp.models.Student
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class StudentActivity : AppCompatActivity() {
+    private lateinit var user_name_tv: TextView
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
+
+        database = FirebaseDatabase.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        //initiate views
+        user_name_tv = findViewById(R.id.user_name_tv)
+
+        //get user name
+        val user_id = auth.currentUser!!.uid
+        database.getReference("students").child(user_id).get().addOnSuccessListener {
+            if (it.exists()) {
+                val student = it.getValue(Student::class.java)
+                user_name_tv.text = getString(R.string.welcome_user, student!!.first_name)
+            }
+        }
     }
 }
