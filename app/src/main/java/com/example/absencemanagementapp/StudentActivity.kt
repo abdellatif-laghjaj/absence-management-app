59f7f262c013f1a60ace61ff544a67396bd6fd16
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-06 14:57:09 +0100

    add student and teacher activity

---
+++ app/src/main/java/com/example/absencemanagementapp/StudentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.absencemanagementapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student)
+    }
+}
