7d51ebb57b57ec611625332c5aa14273e7c47859
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-13 02:12:56 +0100

    get current user data

--- app/src/main/java/com/example/absencemanagementapp/activities/StudentProfileActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/StudentProfileActivity.kt
@@ -6,14 +6,21 @@ import android.util.Patterns
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.absencemanagementapp.R
+import com.example.absencemanagementapp.models.Student
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.shashank.sony.fancytoastlib.FancyToast
 
 class StudentProfileActivity : AppCompatActivity() {
+    private lateinit var user_name_tv: TextView
+    private lateinit var user_email_tv: TextView
     private lateinit var first_name_et: TextInputEditText
     private lateinit var last_name_et: TextInputEditText
     private lateinit var cin_et: TextInputEditText
@@ -21,8 +28,6 @@ class StudentProfileActivity : AppCompatActivity() {
     private lateinit var filiere_dropdown: AutoCompleteTextView
     private lateinit var semester_dropdown: AutoCompleteTextView
     private lateinit var email_et: TextInputEditText
-    private lateinit var password_et: TextInputEditText
-    private lateinit var confirm_password_et: TextInputEditText
     private lateinit var update_btn: Button
 
     private val semesters = arrayOf("1", "2", "3", "4", "5", "6")
@@ -34,9 +39,13 @@ class StudentProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_profile)
 
+        database = FirebaseDatabase.getInstance()
+        auth = FirebaseAuth.getInstance()
+
         //put the code here
         initViews()
         initDropDowns()
+        fillData()
         filiere_dropdown.setOnItemClickListener { adapterView, _, i, _ ->
             //adapterView.getItemAtPosition(i)
         }
@@ -67,8 +76,6 @@ class StudentProfileActivity : AppCompatActivity() {
         val filiere = filiere_dropdown.text.toString()
         val semester = semester_dropdown.text.toString()
         val email = email_et.text.toString()
-        val password = password_et.text.toString()
-        val confirm_password = confirm_password_et.text.toString()
         return when {
             first_name.isEmpty() -> {
                 first_name_et.error = "First name is required"
@@ -110,26 +117,6 @@ class StudentProfileActivity : AppCompatActivity() {
                 email_et.requestFocus()
                 false
             }
-            password.isEmpty() -> {
-                password_et.error = "Password is required"
-                password_et.requestFocus()
-                false
-            }
-            password.length < 6 -> {
-                password_et.error = "Password must be at least 6 characters"
-                password_et.requestFocus()
-                false
-            }
-            confirm_password.isEmpty() -> {
-                confirm_password_et.error = "Confirm password is required"
-                confirm_password_et.requestFocus()
-                false
-            }
-            password != confirm_password -> {
-                confirm_password_et.error = "Passwords don't match"
-                confirm_password_et.requestFocus()
-                false
-            }
             else -> true
         }
     }
@@ -142,6 +129,8 @@ class StudentProfileActivity : AppCompatActivity() {
     }
 
     public fun initViews() {
+        user_name_tv = findViewById(R.id.user_name_tv)
+        user_email_tv = findViewById(R.id.user_email_tv)
         first_name_et = findViewById(R.id.first_name_et)
         last_name_et = findViewById(R.id.last_name_et)
         cin_et = findViewById(R.id.cin_et)
@@ -149,8 +138,35 @@ class StudentProfileActivity : AppCompatActivity() {
         filiere_dropdown = findViewById(R.id.filiere_dropdown)
         semester_dropdown = findViewById(R.id.semester_dropdown)
         email_et = findViewById(R.id.email_et)
-        password_et = findViewById(R.id.password_et)
-        confirm_password_et = findViewById(R.id.confirm_password_et)
         update_btn = findViewById(R.id.update_btn)
+    }
+
+    public fun fillData() {
+        val user = auth.currentUser
+        val userRef = database.getReference("students").child(user!!.uid)
+        userRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val student = snapshot.getValue(Student::class.java)
+                user_name_tv.text = student!!.email
+                user_email_tv.text = student!!.email
+                first_name_et.setText(student!!.first_name)
+                last_name_et.setText(student.last_name)
+                cin_et.setText(student.cin)
+                cne_et.setText(student.cne)
+                filiere_dropdown.setText(student.filiere)
+                semester_dropdown.setText(student.semester)
+                email_et.setText(student.email)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                FancyToast.makeText(
+                    this@StudentProfileActivity,
+                    error.message,
+                    FancyToast.LENGTH_SHORT,
+                    FancyToast.ERROR,
+                    false
+                ).show()
+            }
+        })
     }
 }
