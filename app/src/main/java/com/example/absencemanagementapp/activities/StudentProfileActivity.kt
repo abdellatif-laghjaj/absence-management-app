40025f4f5346830ba781f0c974ad75aa8b426647
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-13 02:03:07 +0100

    profile activity

--- app/src/main/java/com/example/absencemanagementapp/activities/StudentProfileActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/StudentProfileActivity.kt
@@ -2,11 +2,155 @@ package com.example.absencemanagementapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.Toast
 import com.example.absencemanagementapp.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.shashank.sony.fancytoastlib.FancyToast
 
 class StudentProfileActivity : AppCompatActivity() {
+    private lateinit var first_name_et: TextInputEditText
+    private lateinit var last_name_et: TextInputEditText
+    private lateinit var cin_et: TextInputEditText
+    private lateinit var cne_et: TextInputEditText
+    private lateinit var filiere_dropdown: AutoCompleteTextView
+    private lateinit var semester_dropdown: AutoCompleteTextView
+    private lateinit var email_et: TextInputEditText
+    private lateinit var password_et: TextInputEditText
+    private lateinit var confirm_password_et: TextInputEditText
+    private lateinit var update_btn: Button
+
+    private val semesters = arrayOf("1", "2", "3", "4", "5", "6")
+    private val branches = arrayOf("GI", "SV", "LAE", "ECO")
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_profile)
+
+        //put the code here
+        initViews()
+        initDropDowns()
+        filiere_dropdown.setOnItemClickListener { adapterView, _, i, _ ->
+            //adapterView.getItemAtPosition(i)
+        }
+
+        semester_dropdown.setOnItemClickListener { adapterView, _, i, _ ->
+            //adapterView.getItemAtPosition(i)
+        }
+
+        //regsitration logic
+        update_btn.setOnClickListener {
+            if (validateInputs()) {
+                FancyToast.makeText(
+                    this,
+                    "Updating...",
+                    FancyToast.LENGTH_SHORT,
+                    FancyToast.INFO,
+                    false
+                ).show()
+            }
+        }
+    }
+
+    private fun validateInputs(): Boolean {
+        val first_name = first_name_et.text.toString()
+        val last_name = last_name_et.text.toString()
+        val cin = cin_et.text.toString()
+        val cne = cne_et.text.toString()
+        val filiere = filiere_dropdown.text.toString()
+        val semester = semester_dropdown.text.toString()
+        val email = email_et.text.toString()
+        val password = password_et.text.toString()
+        val confirm_password = confirm_password_et.text.toString()
+        return when {
+            first_name.isEmpty() -> {
+                first_name_et.error = "First name is required"
+                first_name_et.requestFocus()
+                false
+            }
+            last_name.isEmpty() -> {
+                last_name_et.error = "Last name is required"
+                last_name_et.requestFocus()
+                false
+            }
+            cin.isEmpty() -> {
+                cin_et.error = "CIN is required"
+                cin_et.requestFocus()
+                false
+            }
+            cne.isEmpty() -> {
+                cne_et.error = "CNE is required"
+                cne_et.requestFocus()
+                false
+            }
+            filiere.isEmpty() -> {
+                filiere_dropdown.error = "Filiere is required"
+                filiere_dropdown.requestFocus()
+                false
+            }
+            semester.isEmpty() -> {
+                semester_dropdown.error = "Semester is required"
+                semester_dropdown.requestFocus()
+                false
+            }
+            email.isEmpty() -> {
+                email_et.error = "Email is required"
+                email_et.requestFocus()
+                false
+            }
+            !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                email_et.error = "Email is not valid"
+                email_et.requestFocus()
+                false
+            }
+            password.isEmpty() -> {
+                password_et.error = "Password is required"
+                password_et.requestFocus()
+                false
+            }
+            password.length < 6 -> {
+                password_et.error = "Password must be at least 6 characters"
+                password_et.requestFocus()
+                false
+            }
+            confirm_password.isEmpty() -> {
+                confirm_password_et.error = "Confirm password is required"
+                confirm_password_et.requestFocus()
+                false
+            }
+            password != confirm_password -> {
+                confirm_password_et.error = "Passwords don't match"
+                confirm_password_et.requestFocus()
+                false
+            }
+            else -> true
+        }
+    }
+
+    private fun initDropDowns() {
+        val filiere_adapter = ArrayAdapter(this, R.layout.dropdown_item, branches)
+        filiere_dropdown.setAdapter(filiere_adapter)
+        val semester_adapter = ArrayAdapter(this, R.layout.dropdown_item, semesters)
+        semester_dropdown.setAdapter(semester_adapter)
+    }
+
+    public fun initViews() {
+        first_name_et = findViewById(R.id.first_name_et)
+        last_name_et = findViewById(R.id.last_name_et)
+        cin_et = findViewById(R.id.cin_et)
+        cne_et = findViewById(R.id.cne_et)
+        filiere_dropdown = findViewById(R.id.filiere_dropdown)
+        semester_dropdown = findViewById(R.id.semester_dropdown)
+        email_et = findViewById(R.id.email_et)
+        password_et = findViewById(R.id.password_et)
+        confirm_password_et = findViewById(R.id.confirm_password_et)
+        update_btn = findViewById(R.id.update_btn)
     }
 }
