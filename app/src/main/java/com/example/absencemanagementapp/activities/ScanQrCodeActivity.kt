19b49bc00abef6340cbfc136e5fbd4f083b5ceaa
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-14 23:08:34 +0100

    start scanning i guess

--- app/src/main/java/com/example/absencemanagementapp/activities/ScanQrCodeActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/ScanQrCodeActivity.kt
@@ -1,14 +1,72 @@
 package com.example.absencemanagementapp.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 import com.example.absencemanagementapp.R
+import com.shashank.sony.fancytoastlib.FancyToast
 
 class ScanQrCodeActivity : AppCompatActivity() {
-    private lateinit var code_scanner_view: CodeScannerView
+    private lateinit var code_scanner: CodeScanner
+    private lateinit var scanner_view: CodeScannerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_qr_code_acticty)
+
+        scanner_view = findViewById(R.id.scanner_view)
+
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
+        } else {
+            //start scanning process
+            startScanning()
+        }
+    }
+
+    private fun startScanning() {
+        code_scanner = CodeScanner(this, scanner_view)
+        code_scanner.camera = CodeScanner.CAMERA_BACK
+        code_scanner.formats = CodeScanner.ALL_FORMATS
+
+        code_scanner.autoFocusMode = AutoFocusMode.SAFE
+        code_scanner.scanMode = ScanMode.SINGLE
+        code_scanner.isAutoFocusEnabled = true
+        code_scanner.isFlashEnabled = false
+
+        code_scanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                FancyToast.makeText(
+                    this,
+                    "Scanned Result: ${it.text}",
+                    FancyToast.LENGTH_LONG,
+                    FancyToast.SUCCESS,
+                    false
+                ).show()
+            }
+        }
+
+        code_scanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                FancyToast.makeText(
+                    this,
+                    "Camera initialization error: ${it.message}",
+                    FancyToast.LENGTH_LONG,
+                    FancyToast.ERROR,
+                    false
+                ).show()
+            }
+        }
     }
 }
