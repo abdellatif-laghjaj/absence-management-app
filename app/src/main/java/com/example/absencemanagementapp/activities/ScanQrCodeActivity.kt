ec809ab4d12a5f9bd297f06d8f2dfe912e86b8ef
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-14 21:57:06 +0100

    add scan qr code activity

---
+++ app/src/main/java/com/example/absencemanagementapp/activities/ScanQrCodeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.absencemanagementapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.absencemanagementapp.R
+
+class ScanQrCodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_qr_code_acticty)
+    }
+}
