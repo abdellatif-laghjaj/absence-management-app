2977e408d097787ade44bf625a0d4a2cf2664100
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-16 22:25:14 +0100

    Update ModulesAdapter.kt

--- app/src/main/java/com/example/absencemanagementapp/adapters/ModulesAdapter.kt
+++ app/src/main/java/com/example/absencemanagementapp/adapters/ModulesAdapter.kt
@@ -9,7 +9,6 @@ import com.example.absencemanagementapp.R
 import com.example.absencemanagementapp.models.Module
 
 class ModulesAdapter(private val mList: List<Module>) : RecyclerView.Adapter<ModulesAdapter.ViewHolder>()  {
-
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var module_intitule: TextView = itemView.findViewById(R.id.module_intitule);
         val module_semestre: TextView = itemView.findViewById(R.id.module_semestre);
@@ -26,6 +25,8 @@ class ModulesAdapter(private val mList: List<Module>) : RecyclerView.Adapter<Mod
         var module = mList[position];
         holder.module_intitule.setText(module.inititule);
         holder.module_semestre.setText(module.semestre.toString());
+
+        holder?.containerView?.setOnClickListener { clickListener(item) }
     }
 
     override fun getItemCount(): Int {
