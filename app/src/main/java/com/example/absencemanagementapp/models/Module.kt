b0118f5d960075efbea984ae57d9866a00f38bfe
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-16 21:03:11 +0100

    Update Module.kt

--- app/src/main/java/com/example/absencemanagementapp/models/Module.kt
+++ app/src/main/java/com/example/absencemanagementapp/models/Module.kt
@@ -1,11 +1,10 @@
 package com.example.absencemanagementapp.models
 
-class Module {
-    var id: Int? = null;
-    var inititule: String? = null;
-    var abrv: String? = null;
-    var semestre: Int? = null;
-    var formation: Int? = null;
-
-    constructor()
+data class Module(
+    var id: Int? = null,
+    var inititule: String? = null,
+    var abrv: String? = null,
+    var semestre: Int? = null,
+    var formation: Int? = null
+) {
 }
