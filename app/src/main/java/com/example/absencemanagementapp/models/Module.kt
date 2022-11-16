41b449d90a14298546458316a15210b44e2f3296
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-16 20:14:52 +0100

    create Module model

---
+++ app/src/main/java/com/example/absencemanagementapp/models/Module.kt
@@ -0,0 +1,11 @@
+package com.example.absencemanagementapp.models
+
+class Module {
+    var id: String? = null;
+    var inititule: String? = null;
+    var abrv: String? = null;
+    var semestre: Int? = null;
+    var formation: Int? = null;
+
+    constructor()
+}
