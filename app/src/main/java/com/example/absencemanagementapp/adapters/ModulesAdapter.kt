dc15588a9e204d3c457412d4fb5239a8452c5100
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-16 21:03:00 +0100

    Create ModulesAdapter.kt

---
+++ app/src/main/java/com/example/absencemanagementapp/adapters/ModulesAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.absencemanagementapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.absencemanagementapp.R
+import com.example.absencemanagementapp.models.Module
+
+class ModulesAdapter(private val mList: List<Module>) : RecyclerView.Adapter<ModulesAdapter.ViewHolder>()  {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var module_intitule: TextView = itemView.findViewById(R.id.module_intitule);
+        val module_semestre: TextView = itemView.findViewById(R.id.module_semestre);
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view_design, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var module = mList[position];
+        holder.module_intitule.setText(module.inititule);
+        holder.module_semestre.setText(module.semestre.toString());
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size;
+    }
+}
