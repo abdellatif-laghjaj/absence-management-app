a59e334bdbe52e46f5fec73fcbeb903aee91e69f
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-17 18:36:22 +0100

    create module view activity

---
+++ app/src/main/java/com/example/absencemanagementapp/activities/ModuleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.absencemanagementapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModuleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_module)
+    }
+}
