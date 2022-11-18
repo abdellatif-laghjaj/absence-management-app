4771fa02415429f444a45b667c332b1c29f3eec0
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-18 17:14:10 +0100

    get id from intent

--- app/src/main/java/com/example/absencemanagementapp/activities/ModuleActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/ModuleActivity.kt
@@ -19,7 +19,6 @@ class ModuleActivity : AppCompatActivity() {
     private lateinit var back_iv: ImageView
     private lateinit var nb_students_tv: TextView
     private lateinit var new_seance_btn: Button
-    var id = 4
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,30 +31,18 @@ class ModuleActivity : AppCompatActivity() {
         // region: Display all seances of module
         val rv = findViewById<RecyclerView>(R.id.seances_rv)
         rv.layoutManager = LinearLayoutManager(this)
-        val seances = ArrayList<Seance>()
-        seances.add(Seance("16/11/2022", "TP", 4))
-        seances.add(Seance("15/11/2022", "Cour", 12))
-        seances.add(Seance("08/11/2022", "Cour", 9))
-        seances.add(Seance("01/11/2022", "Cour", 4))
-        seances.add(Seance("24/10/2022", "TP", 4))
-        seances.add(Seance("17/10/2022", "Cour", 4))
+        val seances = getSeances()
         val seanceAdapter = SeanceAdapter(seances, this)
         rv.adapter = seanceAdapter
         // endregion
     }
 
     private fun initView() {
-        val modules = ArrayList<Module>();
-        modules.add(Module(1, "Algebre 1", "ALG1", 1, 9))
-        modules.add(Module(2, "Analyse 1", "ALG1", 1, 9))
-        modules.add(Module(3, "Physique 1", "ALG1", 1, 9))
-        modules.add(Module(4, "Probabilité statistique", "ALG1", 1, 9))
-        modules.add(Module(5, "Algorithmique et programmation 1", "ALG1", 1, 9))
-        modules.add(Module(6, "Langues et terminologie 1", "ALG1", 1, 9))
-        modules.add(Module(7, "Environnement d'entreprise", "ALG1", 1, 9))
+        var modules = getModules()
 
+        var id = intent.getIntExtra("id", 0)
         module_name_tv = findViewById(R.id.module_intitule_tv)
-        module_name_tv.setText(modules[4].inititule)
+        module_name_tv.setText(modules[id].inititule)
 
         back_iv = findViewById(R.id.back_arrow)
         back_iv.setOnClickListener({ back() })
@@ -65,6 +52,31 @@ class ModuleActivity : AppCompatActivity() {
 
         new_seance_btn = findViewById(R.id.new_seance_btn)
         new_seance_btn.setOnClickListener({ toNewSeanceView() })
+    }
+
+    private fun getModules(): List<Module> {
+        val modules = ArrayList<Module>();
+
+        modules.add(Module(1, "Algebre 1", "ALG1", 1, 9))
+        modules.add(Module(2, "Analyse 1", "ALG1", 1, 9))
+        modules.add(Module(3, "Physique 1", "ALG1", 1, 9))
+        modules.add(Module(4, "Probabilité statistique", "ALG1", 1, 9))
+        modules.add(Module(5, "Algorithmique et programmation 1", "ALG1", 1, 9))
+        modules.add(Module(6, "Langues et terminologie 1", "ALG1", 1, 9))
+        modules.add(Module(7, "Environnement d'entreprise", "ALG1", 1, 9))
+
+        return modules
+    }
+
+    private fun getSeances(): List<Seance> {
+        val seances = ArrayList<Seance>()
+        seances.add(Seance("16/11/2022", "TP", 4))
+        seances.add(Seance("15/11/2022", "Cour", 12))
+        seances.add(Seance("08/11/2022", "Cour", 9))
+        seances.add(Seance("01/11/2022", "Cour", 4))
+        seances.add(Seance("24/10/2022", "TP", 4))
+        seances.add(Seance("17/10/2022", "Cour", 4))
+        return seances
     }
 
     private fun back() {
