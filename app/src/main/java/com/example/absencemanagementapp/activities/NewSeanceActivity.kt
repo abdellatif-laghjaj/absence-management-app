4fae9f57d4fe8ba15ca103760a1cc6720ccefc8e
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-19 17:00:51 +0100

    create new séance activity

---
+++ app/src/main/java/com/example/absencemanagementapp/activities/NewSeanceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.absencemanagementapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewSeanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_seance)
+    }
+}
