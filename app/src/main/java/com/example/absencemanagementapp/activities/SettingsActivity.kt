d116d859d815c984d6c0ba8fc83932873b3e0e8c
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-20 21:30:09 +0100

    create settings activity

---
+++ app/src/main/java/com/example/absencemanagementapp/activities/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.absencemanagementapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.absencemanagementapp.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
