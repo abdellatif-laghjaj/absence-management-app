d277c0c0872e8ff6e6ec7cabff1b1f9aed0a2fe5
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-27 00:53:37 +0100

    fix some issues and improve seance activity 🚀

--- app/src/main/java/com/example/absencemanagementapp/activities/SeanceActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/SeanceActivity.kt
@@ -27,56 +27,33 @@ class SeanceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seance)
 
+        //initiate views
         initView()
+
+        //get seance from intent
+        setDatas()
     }
 
     private fun initView() {
-        val seance_id = intent.getIntExtra("id", 0)
-        val module_intitule = intent.getStringExtra("module_intitule")
-
         module_intitule_tv = this.findViewById(R.id.module_intitule_tv)
-        module_intitule_tv.text = module_intitule
-
         back_iv = findViewById(R.id.back_arrow)
-        back_iv.setOnClickListener { back() }
-
         absence_list_cv = this.findViewById(R.id.absence_list_cv)
-        absence_list_cv.setOnClickListener {
-            showAbsenceList()
-        }
-
         show_qr_code = this.findViewById(R.id.show_qr_code)
-        show_qr_code.setOnClickListener {
-            showQrCode()
-        }
-
         seance_type_tv = this.findViewById(R.id.seance_type_tv)
-        seance_type_tv.text = getCurrentSeance(seance_id).type
-
         seance_date_tv = this.findViewById(R.id.seance_date_tv)
-        seance_date_tv.text = getCurrentSeance(seance_id).date
-
         seance_start_time_tv = this.findViewById(R.id.seance_start_time_tv)
-        seance_start_time_tv.text = getCurrentSeance(seance_id).start_time
-
         seance_end_time_tv = this.findViewById(R.id.seance_end_time_tv)
-        seance_end_time_tv.text = getCurrentSeance(seance_id).end_time
-
         salle_nb_tv = this.findViewById(R.id.salle_nb_tv)
-        salle_nb_tv.text = getCurrentSeance(seance_id).n_salle
-
         total_absence_tv = this.findViewById(R.id.total_absence_tv)
-        total_absence_tv.text = getCurrentSeance(seance_id).total_absences.toString()
     }
 
     private fun getCurrentSeance(id: Int): Seance {
         val seances = ArrayList<Seance>()
-        seances.add(Seance("16/11/2022", "TP", 4))
-        seances.add(Seance("15/11/2022", "Cour", 12))
-        seances.add(Seance("08/11/2022", "Cour", 9))
-        seances.add(Seance("01/11/2022", "Cour", 4))
-        seances.add(Seance("24/10/2022", "TP", 4))
-        seances.add(Seance("17/10/2022", "Cour", 4))
+        seances.add(Seance("16/11/2022", "TP", "08:30", "10:15", "Salle 1", 0))
+        seances.add(Seance("16/11/2022", "Cours", "10:30", "12:15", "Salle 2", 0))
+        seances.add(Seance("16/11/2022", "Exam", "12:30", "14:15", "Salle 3", 0))
+        seances.add(Seance("16/11/2022", "Cours", "14:30", "16:15", "Salle 4", 0))
+        seances.add(Seance("16/11/2022", "TP", "16:30", "18:15", "Salle 5", 0))
         return seances[id]
     }
 
@@ -92,5 +69,18 @@ class SeanceActivity : AppCompatActivity() {
 
     private fun showAbsenceList() {
         println("showAbsenceList")
+    }
+
+    private fun setDatas() {
+        val seance_id = intent.getIntExtra("id", 0)
+        val module_intitule = intent.getStringExtra("module_intitule")
+
+        module_intitule_tv.text = module_intitule
+        seance_type_tv.text = getCurrentSeance(seance_id).type
+        seance_date_tv.text = getCurrentSeance(seance_id).date
+        seance_start_time_tv.text = getCurrentSeance(seance_id).start_time
+        seance_end_time_tv.text = getCurrentSeance(seance_id).end_time
+        salle_nb_tv.text = getCurrentSeance(seance_id).n_salle
+        total_absence_tv.text = getCurrentSeance(seance_id).total_absences.toString()
     }
 }
