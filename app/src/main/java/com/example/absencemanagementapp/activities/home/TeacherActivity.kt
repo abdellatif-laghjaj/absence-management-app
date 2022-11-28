d9728b88fd8306d7bbe578251c5642ce70ad003a
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-28 09:55:35 +0100

    get modules from firebase

--- app/src/main/java/com/example/absencemanagementapp/activities/home/TeacherActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/home/TeacherActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.bitmap_recycle.IntegerArrayAdapter
 import com.example.absencemanagementapp.R
 import com.example.absencemanagementapp.activities.auth.LoginActivity
 import com.example.absencemanagementapp.activities.profile.TeacherProfileActivity
@@ -17,8 +18,12 @@ import com.example.absencemanagementapp.models.Module
 import com.example.absencemanagementapp.models.Teacher
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import de.hdodenhof.circleimageview.CircleImageView
+import java.lang.Integer.parseInt
 
 class TeacherActivity : AppCompatActivity() {
     private lateinit var user_name_tv: TextView
@@ -87,35 +92,53 @@ class TeacherActivity : AppCompatActivity() {
         }
     }
 
-    private fun getModules(): List<Module> {
-        val modules = ArrayList<Module>();
+    private fun getModules() {
 
-        modules.add(Module(1, "Algebre 1", "ALG1", 1, "GI"))
-        modules.add(Module(2, "Analyse 1", "ALG1", 1, "EE"))
-        modules.add(Module(3, "Physique 1", "ALG1", 1, "LEA"))
-        modules.add(Module(4, "Probabilité statistique", "ALG1", 1, "GI"))
-        modules.add(Module(5, "Algorithmique et programmation 1", "ALG1", 1, "SV"))
-        modules.add(Module(6, "Langues et terminologie 1", "ALG1", 1, "EG"))
-        modules.add(Module(7, "Environnement d'entreprise", "ALG1", 1, "SGARNE"))
+        database.getReference("modules").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val modules = ArrayList<Module>();
+                for (ds in snapshot.children) {
+                    val id = parseInt(ds.key.toString())
+                    val inititule = ds.child("intitule").value.toString()
+                    val abrv = ds.child("abrv").value.toString()
+                    val semestre = ds.child("semestre").value.toString()
+                    val formation = ds.child("formation").value.toString()
+                    val respo_id = ds.child("respo_id").value.toString()
+                    var module = Module(id, inititule, abrv, semestre, formation, respo_id)
+                    modules.add(module)
+                }
+                initModules(modules)
+            }
 
-        return modules
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+//        modules.add(Module(1, "Algebre 1", "ALG1", 1, "GI", ""))
+//        modules.add(Module(2, "Analyse 1", "ALG1", 1, "EE", ""))
+//        modules.add(Module(3, "Physique 1", "ALG1", 1, "LEA", ""))
+//        modules.add(Module(4, "Probabilité statistique", "ALG1", 1, "GI", ""))
+//        modules.add(Module(5, "Algorithmique et programmation 1", "ALG1", 1, "SV", ""))
+//        modules.add(Module(6, "Langues et terminologie 1", "ALG1", 1, "EG", ""))
+//        modules.add(Module(7, "Environnement d'entreprise", "ALG1", 1, "SGARNE", ""))
     }
 
-    private fun initModules() {
+    private fun initModules(modules : ArrayList<Module>) {
         val rv = findViewById<RecyclerView>(R.id.module_rv);
         rv.layoutManager = LinearLayoutManager(this);
-        val modulesAdapter = ModulesAdapter(getModules(), this);
+        val modulesAdapter = ModulesAdapter(modules, this);
         rv.adapter = modulesAdapter;
     }
 
     private fun initViews() {
-        initModules()
+        getModules()
         bottom_navigation = findViewById(R.id.bottom_navigation)
         user_name_tv = findViewById(R.id.user_name_tv)
         teacher_image_civ = findViewById(R.id.teacher_image_civ)
         modules_swipe = this.findViewById(R.id.modules_swipe)
         modules_swipe.setOnRefreshListener {
-            initModules()
+            getModules()
             modules_swipe.isRefreshing = false
         }
     }
