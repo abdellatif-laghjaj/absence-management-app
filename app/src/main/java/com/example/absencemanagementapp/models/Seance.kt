09710683b230faa6542f11cf7e4a2cae87709440
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-28 12:09:07 +0100

    Update Seance.kt

--- app/src/main/java/com/example/absencemanagementapp/models/Seance.kt
+++ app/src/main/java/com/example/absencemanagementapp/models/Seance.kt
@@ -1,60 +1,15 @@
 package com.example.absencemanagementapp.models
 
-class Seance {
-    var id: String? = null
-    var date: String? = null
-    var start_time: String? = null
-    var end_time: String? = null
-    var type: String? = "Cour"
-    var n_salle: String? = "A4"
-    var n_module: Int? = 0
+data class Seance(
+    var id: String? = null,
+    var date: String? = null,
+    var start_time: String? = null,
+    var end_time: String? = null,
+    var type: String? = "Cour",
+    var n_salle: String? = "A4",
+    var n_module: Int? = 0,
     var total_absences: Int = 0
-
-    constructor()
-
-
-    constructor(
-        id: String?,
-        date: String?,
-        start_time: String?,
-        end_time: String?,
-        type: String?,
-        branch: String?,
-        n_salle: String?,
-        semester: Int?,
-        n_module: Int?
-    ) {
-        this.id = id
-        this.date = date
-        this.start_time = start_time
-        this.end_time = end_time
-        this.type = type
-        this.n_salle = n_salle
-        this.n_module = n_module
-    }
-
-    constructor(
-        date: String?,
-        start_time: String?,
-        end_time: String?,
-        type: String?,
-        n_salle: String?,
-        n_module: Int?
-    ) {
-        this.date = date
-        this.start_time = start_time
-        this.end_time = end_time
-        this.type = type
-        this.n_salle = n_salle
-        this.n_module = n_module
-    }
-
-    constructor(date: String?, type: String?, total_absences: Int) {
-        this.date = date
-        this.type = type
-        this.total_absences = total_absences
-    }
-
+) {
     override fun toString(): String {
         return "Seance(id=$id, date=$date, start_time=$start_time, end_time=$end_time, type=$type, n_salle=$n_salle, n_module=$n_module, total_absences=$total_absences)"
     }
