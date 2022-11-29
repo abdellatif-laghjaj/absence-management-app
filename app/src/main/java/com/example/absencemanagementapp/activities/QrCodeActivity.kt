3926221c8a9a9d381224b371c67f923b30cabc74
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-29 23:30:04 +0100

    save qr to gallery

--- app/src/main/java/com/example/absencemanagementapp/activities/QrCodeActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/QrCodeActivity.kt
@@ -1,14 +1,17 @@
 package com.example.absencemanagementapp.activities
 
+import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import com.bumptech.glide.Glide
+import android.widget.Toast
 import com.example.absencemanagementapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
@@ -16,7 +19,16 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.shashank.sony.fancytoastlib.FancyToast
 import com.squareup.picasso.Picasso
-import java.net.URI
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.concurrent.Executors
 
 class QrCodeActivity : AppCompatActivity() {
     private lateinit var qr_code_iv: ImageView
@@ -63,7 +75,7 @@ class QrCodeActivity : AppCompatActivity() {
         module_intitule_iv = this.findViewById(R.id.module_intitule_iv)
 
         back_iv.setOnClickListener { back() }
-        share_btn.setOnClickListener { shareAction() }
+        share_btn.setOnClickListener { saveQrCode() }
     }
 
     //get qr code image from firebase storage
@@ -80,7 +92,63 @@ class QrCodeActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun shareAction() {
-//        TODO("Not yet implemented")
+    private fun saveQrCode() {
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+        var image: Bitmap?
+
+        executor.execute {
+            image = loadImage(url)
+            handler.post {
+                image?.let { saveMediaToStorage(it) }
+            }
+        }
+    }
+
+    private fun loadImage(string: String?) : Bitmap? {
+        var url: URL?
+        val connection: HttpURLConnection?
+        try {
+            url = URL(string)
+            try {
+                connection = url.openConnection() as HttpURLConnection
+                connection.connect()
+                val inputStream: InputStream = connection.inputStream
+                val bufferedInputStream = BufferedInputStream(inputStream)
+                connection.disconnect()
+                return BitmapFactory.decodeStream(bufferedInputStream)
+            } catch (e: IOException) {
+                e.message?.let { Log.e("connection error", it) }
+                Toast.makeText(this, "problem in connection!!", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: MalformedURLException) {
+            url = null
+            Toast.makeText(this, "image uri mal formed!", Toast.LENGTH_SHORT).show()
+        }
+        return null
+    }
+
+    private fun saveMediaToStorage(bitmap: Bitmap) {
+        val filename = "${module_intitule}_${seance_id}_${System.currentTimeMillis()}.jpg"
+        var fos: OutputStream? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            this.contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, "${Environment.DIRECTORY_PICTURES}")
+                }
+                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+        fos?.use {
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            FancyToast.makeText(this, "Saved to gallery", Toast.LENGTH_SHORT).show()
+        }
     }
 }
