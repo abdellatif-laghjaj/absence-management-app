da1ec0383a7a41d11bccc87360fd45a8f94a39dc
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-29 12:45:29 +0100

    Update QrCodeActivity.kt

--- app/src/main/java/com/example/absencemanagementapp/activities/QrCodeActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/QrCodeActivity.kt
@@ -3,8 +3,10 @@ package com.example.absencemanagementapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.absencemanagementapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -12,6 +14,9 @@ import com.shashank.sony.fancytoastlib.FancyToast
 
 class QrCodeActivity : AppCompatActivity() {
     private lateinit var qr_code_iv: ImageView
+    private lateinit var back_iv: ImageView
+    private lateinit var share_btn: FloatingActionButton
+    private lateinit var module_intitule_iv: TextView
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
@@ -25,7 +30,7 @@ class QrCodeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_code)
 
-        qr_code_iv = findViewById(R.id.qr_code_iv)
+        initView()
 
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
@@ -33,20 +38,31 @@ class QrCodeActivity : AppCompatActivity() {
 
         //get seance id and module id from intent
         seance_id = intent.getStringExtra("seance_id").toString()
-        module_id = intent.getStringExtra("module_id").toString()
+        module_id = intent.getIntExtra("module_id", -1).toString()
 
         //get qr code
-        getQrCodeImage()
+        setQrCodeImage()
+    }
+
+    private fun initView() {
+        qr_code_iv = this.findViewById(R.id.qr_code_iv)
+        back_iv = this.findViewById(R.id.back_iv)
+        share_btn = this.findViewById(R.id.share_btn)
+        module_intitule_iv = this.findViewById(R.id.module_intitule_iv)
+
+        back_iv.setOnClickListener { back() }
+        share_btn.setOnClickListener { shareAction() }
     }
 
     //get qr code image from firebase storage
-    private fun getQrCodeImage() {
+    private fun setQrCodeImage() {
         val storageRef = storage.reference
         val qrCodeRef = storageRef.child("qr_codes").child(module_id).child(seance_id)
 
         qrCodeRef.downloadUrl.addOnSuccessListener {
             Glide.with(this).load(it).into(qr_code_iv)
             println("qr code url ===> $it")
+            qr_code_iv.setImageURI(it)
         }.addOnFailureListener {
             FancyToast.makeText(
                 this,
@@ -56,5 +72,13 @@ class QrCodeActivity : AppCompatActivity() {
                 false
             ).show()
         }
+    }
+
+    private fun back() {
+        TODO("not implemented yet!!")
+    }
+
+    private fun shareAction() {
+        TODO("Not yet implemented")
     }
 }
