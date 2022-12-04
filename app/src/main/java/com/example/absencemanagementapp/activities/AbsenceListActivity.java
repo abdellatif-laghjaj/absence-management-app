c1a093e289a1b3546af08a8fd968bc0fb14b7662
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-05 00:13:04 +0100

    create list

--- app/src/main/java/com/example/absencemanagementapp/activities/AbsenceListActivity.java
+++ app/src/main/java/com/example/absencemanagementapp/activities/AbsenceListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.absencemanagementapp.R;
+
 public class AbsenceListActivity extends AppCompatActivity {
 
     @Override
