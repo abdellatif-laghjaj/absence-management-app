2038133f7aef93a6abed41cfc4d12d03b6b3e092
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-05 00:24:43 +0100

    Update AbsenceListActivity.kt

--- app/src/main/java/com/example/absencemanagementapp/activities/AbsenceListActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/AbsenceListActivity.kt
@@ -1,10 +1,20 @@
 package com.example.absencemanagementapp.activities
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.example.absencemanagementapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class AbsenceListActivity : AppCompatActivity() {
+    private lateinit var absence_list_rv: RecyclerView
+    private lateinit var back_iv: ImageView
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_absence_list)
