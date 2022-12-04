678b92f680914b38b9231aa2bcebf1ad4f604597
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-05 00:06:37 +0100

    create absence list activity

---
+++ app/src/main/java/com/example/absencemanagementapp/activities/AbsenceListActivity.java
@@ -0,0 +1,14 @@
+package com.example.absencemanagementapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AbsenceListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absence_list);
+    }
+}
