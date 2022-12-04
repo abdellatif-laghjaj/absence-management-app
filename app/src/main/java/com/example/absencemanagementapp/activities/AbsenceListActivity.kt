0b49e241af14e72885e365f87533c6a0e7daa689
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-05 00:36:45 +0100

    some bug fixes

--- app/src/main/java/com/example/absencemanagementapp/activities/AbsenceListActivity.kt
+++ app/src/main/java/com/example/absencemanagementapp/activities/AbsenceListActivity.kt
@@ -3,8 +3,11 @@ package com.example.absencemanagementapp.activities
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.absencemanagementapp.R
+import com.example.absencemanagementapp.adapters.AbsenceAdapter
+import com.example.absencemanagementapp.models.Absence
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
@@ -12,11 +15,70 @@ class AbsenceListActivity : AppCompatActivity() {
     private lateinit var absence_list_rv: RecyclerView
     private lateinit var back_iv: ImageView
 
+    private lateinit var absence_adapter: AbsenceAdapter
+
     private lateinit var database: FirebaseDatabase
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_absence_list)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+        //initiate views
+        initViews()
+
+        back_iv.setOnClickListener { back() }
+
+        //initiate absence adapter
+        val absence_list = mutableListOf<Absence>()
+        absence_list.add(Absence("cne 1", "188q7872", true))
+        absence_list.add(Absence("cne 2", "188q7872", true))
+        absence_list.add(Absence("cne 3", "188q7872", false))
+        absence_list.add(Absence("cne 4", "188q7872", true))
+        absence_list.add(Absence("cne 5", "188q7872", true))
+        absence_list.add(Absence("cne 6", "188q7872", false))
+        absence_list.add(Absence("cne 7", "188q7872", false))
+        absence_list.add(Absence("cne 8", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+        absence_list.add(Absence("cne 9", "188q7872", true))
+
+
+        absence_adapter = AbsenceAdapter(absence_list, this)
+        absence_list_rv.adapter = absence_adapter
+        absence_list_rv.setHasFixedSize(true)
+        absence_list_rv.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun back() {
+        finish()
+    }
+
+
+    private fun initViews() {
+        absence_list_rv = findViewById(R.id.absence_list_rv)
+        back_iv = findViewById(R.id.back_arrow)
     }
 }
