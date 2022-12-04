bc48db56cc04f8bfb0858ba301127db269d2b557
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-05 00:23:30 +0100

    fix some issues

---
+++ app/src/main/java/com/example/absencemanagementapp/activities/AbsenceListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.absencemanagementapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.absencemanagementapp.R
+
+class AbsenceListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_absence_list)
+    }
+}
